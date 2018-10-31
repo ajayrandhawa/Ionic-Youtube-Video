bf5f3ac32c9c190415bb8173c355730eef3b9655
rtgroups <ajayrandhawartg@gmail.com>
2018-10-31 17:56:15 +0530

    testing commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  url;
 
+  constructor(public navCtrl: NavController, private sanitizer: DomSanitizer) {
+    this.getSafeUrl("https://www.youtube.com/embed/S46MFd-JdK4");
   }
 
+  getSafeUrl(url) {
+		this.url = this.sanitizer.bypassSecurityTrustResourceUrl(url);		
+	}
 }
